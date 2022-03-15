9a1d09decf51ab57c137fc445f314694572d04e6
caroline tomomi <c.parinsfukuchi@utoronto.ca>
2022-03-15 15:48:04 -0400

    wk3 dist

---
+++ week3/distributions.py
@@ -0,0 +1,12 @@
+import math
+
+class normal:
+    def __init__(self, mean = 0.0, sd = 1.0):
+        self.mean = mean
+        self.sd = sd
+
+    def pdf(self, x):
+        # HINT: to calculate the pdf, you will need math.e, math.pi, and math.sqrt
+        density = 0 # here, you will plug the value X into the PDF for a normal distribution
+        return density
+
